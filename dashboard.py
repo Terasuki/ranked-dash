3f01bd6b2477a561d01b50494f281a32edb85254
Terasuki <xt.wanglin@gmail.com>
2023-08-29 11:31:02 +0200

    requirements

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,6 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import dash_bootstrap_components as dbc
 from sklearn.preprocessing import MultiLabelBinarizer
 import plotly.graph_objects as go
