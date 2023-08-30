9056189e05e342daad635f710153bbdd6c554ab8
Terasuki <xt.wanglin@gmail.com>
2023-08-30 15:56:28 +0200

    fix

--- dashboard.py
+++ dashboard.py
@@ -288,4 +288,4 @@ app.layout = dbc.Container(
 )
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from memory_profiler import profile
-
-@profile
-def load_data():
-    return pd.read_parquet('./data.parquet')
-
-if __name__ == '__main__':
-    X = load_data()
